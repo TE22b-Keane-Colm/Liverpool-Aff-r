eb2544e1fa84947f27f23325e73eb4f82c43a783
TE22b-Keane-Colm <colm.keane@elev.ga.ntig.se>
2024-03-05 14:38:18 +0100

    gjort javascript

--- skript.js
+++ skript.js
@@ -1,3 +1,18 @@
-const köp = document.querySelectorall(`button(type="button")`);
-const produktList = document.querySelector(`ul`);
-let produktarray= [];
+let total = 0;
+function addKundvagn(fotboll,pris)
+{
+  console.log(fotboll, " ", pris);
+
+
+      total += pris;
+  document.querySelector("#vagn-items").innerHTML +=
+"<li>" + fotboll + " - " + pris + " kr</li>";
+  document.getElementById("vagn-total").textContent = total.toFixed(2);
+
+}
+function tabortvagn(params)
+ {
+  document.querySelector("#vagn-items").innerHTML = ""; 
+  document.getElementById("vagn-total").textContent = 0;
+  total = 0;
+}
